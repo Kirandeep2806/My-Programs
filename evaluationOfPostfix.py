aae7c2eb81021204e2679891602f8d846ee7f089
Kiran Deep <kirandeep102030@gmail.com>
2021-12-05 11:17:02 +0530

    Added evaluation of Postfix

---
+++ evaluationOfPostfix.py
@@ -0,0 +1,62 @@
+import numpy as np
+from math import ceil
+
+expr = input("Enter the expression : ")
+size = ceil(len(expr)/2)
+
+class Stack:
+    def __init__(self, stackSize):
+        self.tos = -1
+        self.stackSize = stackSize
+        self.stack = np.array([None]*size)
+
+    def precedence(self, op):
+        if op == "^":
+            return 3
+        elif op == "/" or op == "*" or op == "%":
+            return 2
+        elif op == "+" or op == "-":
+            return 1
+        else:
+            return 0
+
+    def isOperator(self, op):
+        if op == "^" or op == "/" or op == "*" or op == "%" or op == "+" or op == "-":
+            return True
+        return False
+    
+    def push(self, data):
+        if self.isOperator(data):
+            if data == "+":
+                operation_res = self.stack[self.tos-1] + self.stack[self.tos]
+            elif data == "-":
+                operation_res = self.stack[self.tos-1] - self.stack[self.tos]
+            elif data == "/":
+                operation_res = self.stack[self.tos-1] / self.stack[self.tos]
+            elif data == "*":
+                operation_res = self.stack[self.tos-1] * self.stack[self.tos]
+            elif data == "%":
+                operation_res = self.stack[self.tos-1] % self.stack[self.tos]
+            else:
+                operation_res = self.stack[self.tos-1] ** self.stack[self.tos]
+            self.pop()
+            self.stack[self.tos] = operation_res
+        else:
+            self.tos += 1
+            self.stack[self.tos] = int(data)
+
+    def pop(self):
+        if self.tos == -1:
+            print("Stack Underflow")
+        else:
+            self.stack[self.tos] = None
+            self.tos -= 1
+
+    def getRes(self):
+        return self.stack[self.tos]
+
+
+s = Stack(size)
+for i in expr:
+    s.push(i)
+print(s.getRes())
