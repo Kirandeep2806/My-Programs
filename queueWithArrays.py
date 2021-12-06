4c6026f18615e11c25dcf434e944dd6ecc38dbd3
Kiran Deep <kirandeep102030@gmail.com>
2021-12-06 19:06:39 +0530

    Added ADT of queues

---
+++ queueWithArrays.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+class Queue:
+    def __init__(self) -> None:
+        pass
+
+    def enqueue(self, data: int) -> None:
+        pass
+
+    def dequeue(self) -> int:
+        pass
+
+    def size(self) -> int:
+        pass
+
+    def isEmpty(self) -> bool:
+        pass
+
+
