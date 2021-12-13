5cc04856a06702f6ab70dc5ca24f9f72db3658fb
Kiran Deep <kirandeep102030@gmail.com>
2021-12-13 21:18:51 +0530

    Added Day 1 code

---
+++ Hackerrank 30 days code/Day 1.py
@@ -0,0 +1,15 @@
+from sys import stdin, stdout
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+mi = int(stdin.readline())
+md = float(stdin.readline())
+ms = stdin.readline()
+
+stdout.write(str(mi+i))
+stdout.write("\n")
+stdout.write(str(md+d))
+stdout.write("\n")
+stdout.write(str(s+ms))
