ec8d87ca339afb93adeb2320d61cf340720e68b2
Kiran Deep <kirandeep102030@gmail.com>
2021-12-15 21:11:51 +0530

    Added deletion operation on BST

--- binarySearchTree.py
+++ binarySearchTree.py
@@ -32,11 +32,23 @@ class BST:
             else:
                 pos.right = Node(data)
 
-    def traverseTree(self, root: Node) -> None:
+    def preorder(self, root: Node) -> None:
         if root:
             print(root.data, end=" ")
-            self.traverseTree(root.left)
-            self.traverseTree(root.right)
+            self.preorder(root.left)
+            self.preorder(root.right)
+
+    def inorder(self, root: Node) -> None:
+        if root:
+            self.inorder(root.left)
+            print(root.data, end=" ")
+            self.inorder(root.right)
+
+    def postorder(self, root: Node) -> None:
+        if root:
+            self.postorder(root.left)
+            self.postorder(root.right)
+            print(root.data, end=" ")
 
     def search(self, data: int, root: Node) -> Node:
         if root:
@@ -49,18 +61,49 @@ class BST:
                     return self.search(data, root.right)
         return "Element {} is not present in the tree".format(data)
 
+    def findMax(self, root: Node) -> Node:
+        if root is None or root.right is None:
+            return None
+        self.findMax(root.right)
+
+    def deleteNode(self, root: Node, data: int) -> Node:
+        if root:
+            if root.data<data:
+                root.right = self.deleteNode(root.right, data)
+            elif root.data>data:
+                root.left = self.deleteNode(root.left, data)
+            else:
+                if root.left and root.right:
+                    temp = self.findMax(root.left)
+                    root.data = temp.data
+                    root.left = self.deleteNode(root.left, temp.data)
+                elif root.left:
+                    root = root.left
+                else:
+                    root = root.right
+        return root
+
 
 treeObj = BST()
 while True:
-    choice = int(input("1. Insert\t2. Traverse\t3. Search\t4. Exit\nEnter your choice : "))
+    choice = int(input("1. Insert\t2. Preorder Traverse\t3. Inorder Traverse\t4. Postorder Traverse\t5. Search\t6. Delete\t7. Exit\nEnter your choice : "))
     if choice == 1:
         data = int(input("Enter the input to be inserted : "))
         treeObj.insert(data)
     elif choice == 2:
-        treeObj.traverseTree(treeObj.root)
+        treeObj.preorder(treeObj.root)
         print()
     elif choice == 3:
+        treeObj.inorder(treeObj.root)
+        print()
+    elif choice == 4:
+        treeObj.postorder(treeObj.root)
+        print()
+    elif choice == 5:
         data = int(input("Enter the data to be searched : "))
         print(treeObj.search(data, treeObj.root))
+    elif choice == 6:
+        data = int(input("Enter the data to be deleted : "))
+        treeObj.deleteNode(treeObj.root, data)
     else:
         quit(0)
