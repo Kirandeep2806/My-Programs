315313361257de6c5c88f72a66670502acf95111
Kiran Deep <kirandeep102030@gmail.com>
2021-12-22 11:04:18 +0530

    Added Day 12 Code(local runnable file final)

--- Hackerrank 30 days code/Day12.java
+++ Hackerrank 30 days code/Day12.java
@@ -1,94 +1,88 @@
 import java.util.*;
 
 class Person {
-	protected String firstName;
-	protected String lastName;
-	protected int idNumber;
+    protected String firstName;
+    protected String lastName;
+    protected int idNumber;
 
-	// Constructor
-	Person(String firstName, String lastName, int identification) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.idNumber = identification;
-	}
+    // Constructor
+    Person(String firstName, String lastName, int identification) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = identification;
+    }
 
-	// Print person data
-	public void printPerson() {
-		System.out.println(
-				"Name: " + lastName + ", " + firstName
-						+ "\nID: " + idNumber);
-	}
+    // Print person data
+    public void printPerson() {
+        System.out.println(
+                "Name: " + lastName + ", " + firstName
+                        + "\nID: " + idNumber);
+    }
 
 }
 
 class Day12 extends Person { // class Day12 extends Person
-	private int[] testScores;
+    private int[] testScores;
 
-	public Day12(String firstName, String lastName, int identification, int[] testScores) { // public Day12
-		super(firstName, lastName, identification);
-		this.testScores = testScores;
-	}
+    public Day12(String firstName, String lastName, int identification, int[] testScores) { // public Day12
+        super(firstName, lastName, identification);
+        this.testScores = testScores;
+    }
 
-	public char calculate() {
-		int sum = 0, score = 0;
-		for (int i : this.testScores)
-			sum += i;
-		// System.out.println(i);
-		score = sum / this.testScores.length;
-		if (90 <= score && score <= 100)
-			return 'O';
-		else if (80 <= score && score < 90)
-			return 'E';
-		else if (70 <= score && score < 80)
-			return 'A';
-		else if (55 <= score && score < 70)
-			return 'P';
-		else if (40 <= score && score < 55)
-			return 'D';
-		else
-			return 'T';
-	}
+    public char calculate() {
+        int sum = 0, score = 0;
+        for (int i : this.testScores)
+            sum += i;
+        // System.out.println(i);
+        score = sum / this.testScores.length;
+        if (90 <= score && score <= 100)
+            return 'O';
+        else if (80 <= score && score < 90)
+            return 'E';
+        else if (70 <= score && score < 80)
+            return 'A';
+        else if (55 <= score && score < 70)
+            return 'P';
+        else if (40 <= score && score < 55)
+            return 'D';
+        else
+            return 'T';
+    }
 
-	/*
-	 * Class Constructor
-	 * 
-	 * @param firstName - A string denoting the Person's first name.
-	 * 
-	 * @param lastName - A string denoting the Person's last name.
-	 * 
-	 * @param id - An integer denoting the Person's ID number.
-	 * 
-	 * @param scores - An array of integers denoting the Person's test scores.
-	 */
-	// Write your constructor here
+    /*
+     * Class Constructor
+     * 
+     * @param firstName - A string denoting the Person's first name.
+     * 
+     * @param lastName - A string denoting the Person's last name.
+     * 
+     * @param id - An integer denoting the Person's ID number.
+     * 
+     * @param scores - An array of integers denoting the Person's test scores.
+     */
+    // Write your constructor here
 
-	/*
-	 * Method Name: calculate
-	 * 
-	 * @return A character denoting the grade.
-	 */
-	// Write your method here
-}
+    /*
+     * Method Name: calculate
+     * 
+     * @return A character denoting the grade.
+     */
+    // Write your method here
 
-<<<<<<< HEAD
-class Day12 {
-=======
-class Solution {
->>>>>>> a0faad12051e99dd542ddd9a836af4356f51636e
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		String firstName = scan.next();
-		String lastName = scan.next();
-		int id = scan.nextInt();
-		int numScores = scan.nextInt();
-		int[] testScores = new int[numScores];
-		for (int i = 0; i < numScores; i++) {
-			testScores[i] = scan.nextInt();
-		}
-		scan.close();
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String firstName = scan.next();
+        String lastName = scan.next();
+        int id = scan.nextInt();
+        int numScores = scan.nextInt();
+        int[] testScores = new int[numScores];
+        for (int i = 0; i < numScores; i++) {
+            testScores[i] = scan.nextInt();
+        }
+        scan.close();
 
-		Student s = new Student(firstName, lastName, id, testScores);
-		s.printPerson();
-		System.out.println("Grade: " + s.calculate());
-	}
+        Day12 s = new Day12(firstName, lastName, id, testScores);
+        s.printPerson();
+        System.out.println("Grade: " + s.calculate());
+    }
 }
