da09cb20516dd6fe9c9d780b22b14fcc3bd926ed
Kiran Deep <kirandeep102030@gmail.com>
2021-12-23 21:35:44 +0530

    Updated Day 13 code(Java)

--- Hackerrank 30 days code/Day13.java
+++ Hackerrank 30 days code/Day13.java
@@ -3,55 +3,54 @@ import java.util.*;
 abstract class Book {
     String title;
     String author;
-    
+
     Book(String title, String author) {
         this.title = title;
         this.author = author;
     }
-    
+
     abstract void display();
 }
 
-class MyBook extends Book{
+class MyBook extends Book {
     int price;
-    
-    MyBook(String title, String author, int price){
+
+    MyBook(String title, String author, int price) {
         super(title, author);
         this.price = price;
     }
-    
-    public void display(){
+
+    public void display() {
         System.out.println(
                 "Title: " + this.title + "\n"
-            +   "Author: " + this.author + "\n"
-            +   "Price: " + this.price
-        );
+                        + "Author: " + this.author + "\n"
+                        + "Price: " + this.price);
     }
 }
 
 // Declare your class here. Do not use the 'public' access modifier.
-    // Declare the price instance variable
-    
-    /**   
-    *   Class Constructor
-    *   
-    *   @param title The book's title.
-    *   @param author The book's author.
-    *   @param price The book's price.
-    **/
-    // Write your constructor here
-    
-    /**   
-    *   Method Name: display
-    *   
-    *   Print the title, author, and price in the specified format.
-    **/
-    // Write your method here
-    
+// Declare the price instance variable
+
+/**
+ * Class Constructor
+ * 
+ * @param title  The book's title.
+ * @param author The book's author.
+ * @param price  The book's price.
+ **/
+// Write your constructor here
+
+/**
+ * Method Name: display
+ * 
+ * Print the title, author, and price in the specified format.
+ **/
+// Write your method here
+
 // End class
 
-public class Solution {
-   
+public class Day13 { // public class Solution
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String title = scanner.nextLine();
