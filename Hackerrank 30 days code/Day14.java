1046e69fe4c483d6bba32455392ff89c4e09d4f9
Kiran Deep <kirandeep102030@gmail.com>
2021-12-24 20:32:01 +0530

    Added Day 14 code

---
+++ Hackerrank 30 days code/Day14.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Difference {
+    private int[] elements;
+    public int maximumDifference = Integer.MIN_VALUE;
+
+    Difference(int[] a) {
+        this.elements = a;
+    }
+
+    public void computeDifference() {
+        int temp = 0;
+        for (int i = 0; i < this.elements.length - 1; i++) {
+            for (int j = i + 1; j < this.elements.length; j++) {
+                temp = Math.abs(this.elements[i] - this.elements[j]);
+                if (temp > maximumDifference)
+                    maximumDifference = temp;
+            }
+        }
+    }
+
+}
+
+public class Day14 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+
+        difference.computeDifference();
+
+        System.out.print(difference.maximumDifference);
+    }
+}
