3883feffdbe7080233523556567193e35287732f
Kiran Deep <kirandeep102030@gmail.com>
2021-12-25 22:53:25 +0530

    Added Day 15 code

---
+++ Hackerrank 30 days code/Day 15.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+class Node {
+	int data;
+	Node next;
+	Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class Solution {
+    public static  Node insert(Node head,int data) {
+        Node newNode = new Node(data);
+        Node currHead, prevNode;
+        currHead = prevNode = head;
+        if(head == null)
+            return newNode;
+        else {
+            while(currHead != null){
+                prevNode = currHead;
+                currHead = currHead.next;
+            }
+            prevNode.next = newNode;
+        }
+        return head;
+    }
+
+	public static void display(Node head) {
+        Node start = head;
+        while(start != null) {
+            System.out.print(start.data + " ");
+            start = start.next;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+        int N = sc.nextInt();
+
+        while(N-- > 0) {
+            int ele = sc.nextInt();
+            head = insert(head,ele);
+        }
+        display(head);
+        sc.close();
+    }
+}
