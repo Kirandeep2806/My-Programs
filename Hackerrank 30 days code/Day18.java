3f97c79eff8107a9b8b8a1a1c619b214e13b5bd2
Kiran Deep <kirandeep102030@gmail.com>
2021-12-28 18:53:34 +0530

    Added Day 18 code

--- Hackerrank 30 days code/Day18.java
+++ Hackerrank 30 days code/Day18.java
@@ -2,22 +2,22 @@ import java.io.*;
 import java.util.*;
 
 public class Day18 { // public class Solution
-    
+
     Queue<Character> q = new ArrayDeque<>();
     Stack<Character> s = new Stack<>();
-    
+
     public void pushCharacter(char ch) {
         s.push(ch);
     }
-    
+
     public void enqueueCharacter(char ch) {
         q.offer(ch);
     }
-    
+
     public char popCharacter() {
         return s.pop();
     }
-    
+
     public char dequeueCharacter() {
         return q.poll();
     }
@@ -41,15 +41,15 @@ public class Day18 { // public class Solution
 
         // Pop/Dequeue the chars at the head of both data structures and compare them:
         boolean isPalindrome = true;
-        for (int i = 0; i < s.length/2; i++) {
+        for (int i = 0; i < s.length / 2; i++) {
             if (p.popCharacter() != p.dequeueCharacter()) {
-                isPalindrome = false;                
+                isPalindrome = false;
                 break;
             }
         }
 
-        //Finally, print whether string s is palindrome or not.
-        System.out.println( "The word, " + input + ", is " 
-                           + ( (!isPalindrome) ? "not a palindrome." : "a palindrome." ) );
+        // Finally, print whether string s is palindrome or not.
+        System.out.println("The word, " + input + ", is "
+                + ((!isPalindrome) ? "not a palindrome." : "a palindrome."));
     }
 }
