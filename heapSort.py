00a460c18d9e7cf1f55d1d85876e955690824238
Kiran Deep <kirandeep102030@gmail.com>
2021-12-31 09:55:09 +0530

    Added Heap Sort

---
+++ heapSort.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+def heapify(arr, n, i):
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 2
+
+    if l < n and arr[largest] < arr[l]:
+        largest = l
+
+    if r < n and arr[largest] < arr[r]:
+        largest = r
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
+
+def heapSort(arr):
+    n = len(arr)
+
+    for i in range(n//2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n-1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+
+
+arr = [int(i) for i in input("Enter the numbers : ").split()]
+heapSort(arr)
+n = len(arr)
+print("Sorted array is : ", end="")
+
+for i in range(n):
+    print(arr[i], end=" ")
