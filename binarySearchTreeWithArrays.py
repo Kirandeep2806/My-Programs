c9241586848c7bfde4102f65e3dd227ad5688018
Kiran Deep <kirandeep102030@gmail.com>
2022-01-18 23:42:54 +0530

    Added binary search tree with arrays

---
+++ binarySearchTreeWithArrays.py
@@ -0,0 +1,15 @@
+a = [1,2,3]
+h = len(a)-1
+res = [None]*(2**(h+1)-1)
+
+def findIndexOfInsertion(root, insertingElement):
+    if res[root] is None:
+        return root
+    elif insertingElement > res[root]:
+        return findIndexOfInsertion(2*root+2, insertingElement)
+    return findIndexOfInsertion(2*root+1, insertingElement)
+
+for i in a:
+    res[findIndexOfInsertion(0, i)] = i
+
+print(res)
