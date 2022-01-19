6db1d485501bd38872eb1c875e9f136dbc9daf9b
Kiran Deep <kirandeep102030@gmail.com>
2022-01-19 08:47:32 +0530

    Modified the createGraph to be used as imported file

--- createGraph.py
+++ createGraph.py
@@ -6,12 +6,11 @@ from typing import List
 def defaultValueOfGraph() -> List:
     return []
 
-graph = defaultdict(defaultValueOfGraph) # This can also be replace with defaultdict(list)
-print(graph)
+def createGraph():
+    graph = defaultdict(defaultValueOfGraph) # This can also be replace with defaultdict(list)
 
-# Programmed in a way that the nodes are of numeric type i.e., 1,2,3,4,......
-for i in range(1, int(input("Enter the no.of nodes you are having : "))+1):
-    graph[i] = list(map(int, input("Enter the adjacent nodes of node {} : ".format(i)).split()))
+    # Programmed in a way that the nodes are of numeric type i.e., 1,2,3,4,......
+    for i in range(1, int(input("Enter the no.of nodes you are having : "))+1):
+        graph[i] = list(map(int, input("Enter the adjacent nodes of node {} : ".format(i)).split()))
 
-print(graph[4])
-
+    return graph
