236a2ab76b4e4f9d0f2744762c9a4050d2c3e9ac
Kiran Deep <kirandeep102030@gmail.com>
2022-01-24 22:59:04 +0530

    Added Number of 1 Bits code

---
+++ GFG/Number of 1 Bits.cpp
@@ -0,0 +1,33 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+  public:
+    int setBits(int N) {
+        int count=0;
+        while(N){
+            if(N&1)
+                count += 1;
+            N >>= 1;
+        }
+        return count;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+
+        Solution ob;
+        int cnt = ob.setBits(N);
+        cout << cnt << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
