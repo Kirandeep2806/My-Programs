5d5069020cdfd41b0565db3db9bcaaaf5c17a7cb
Kiran Deep <kirandeep102030@gmail.com>
2022-01-24 22:57:42 +0530

    Added Longest Consecutive 1's code

---
+++ GFG/Longest Consecutive 1's.cpp
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+/*  Function to calculate the longest consecutive ones
+*   N: given input to calculate the longest consecutive ones
+*/
+class Solution
+{
+    public:
+    int maxConsecutiveOnes(int N) {
+        int max=0, prev=0;
+        while(N) {
+            if(N&1)
+                prev += 1;
+            else
+                prev = 0;
+            if(prev > max)
+                max = prev;
+            N >>= 1;
+        }
+        return max;
+    }
+};
+
+
+// { Driver Code Starts.
+
+// Driver Code
+int main() {
+	int t;
+	cin>>t;//testcases
+	while(t--)
+	{
+		int n;
+		cin>>n;//input n
+		Solution obj;
+		//calling maxConsecutiveOnes() function
+		cout<<obj.maxConsecutiveOnes(n)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
