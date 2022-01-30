4641e3ef6780b896ba711e061d424e7da2859046
Kiran Deep <kirandeep102030@gmail.com>
2022-01-30 14:21:49 +0530

    Added Nuts and Bolts Problems

---
+++ GFG/Nuts and Bolts Problem.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+#User function Template for python3
+class Solution:
+
+    def matchPairs(self,nuts, bolts, n):
+        l = {"!":0, "#":1, "$":2, "%":3, "&":4, "*":5, "@":6, "^":7, "~":8}
+        res = [False]*9
+        for i in nuts:
+            res[l[i]] = i
+        start = 0
+        for i in res:
+            if i:
+                nuts[start] = i
+                bolts[start] = i
+                start += 1
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n = int(input())
+        nuts = list(map(str, input().strip().split()))
+        bolts = list(map(str, input().strip().split()))
+        ob = Solution()
+        ob.matchPairs(nuts, bolts, n)
+        for x in nuts:
+            print(x, end=" ")
+        print()
+        for x in bolts:
+            print(x, end=" ")
+        print()
+        tc -= 1
+
+# } Driver Code Ends
