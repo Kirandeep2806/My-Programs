7ec51ab650bc2f00ad77610bd336d1d6675712f8
Kiran Deep <kirandeep102030@gmail.com>
2022-02-08 20:05:37 +0530

    Added Check set bits

---
+++ GFG/Check set bits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+#User function Template for python3
+class Solution:
+    def isBitSet (self, N):
+        if N == 0:
+            return 0
+        elif ((N+1)&N) == 0:
+            return 1
+        else:
+            return 0
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        N = int(input())
+       
+
+        ob = Solution()
+        print(ob.isBitSet(N))
+# } Driver Code Ends
