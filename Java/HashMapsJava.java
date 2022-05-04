0cb3037e00e0e0fd14318395e150f1e9320c2ffe
Kiran Deep <kirandeep102030@gmail.com>
2022-05-04 20:11:52 +0530

    Created Hashmaps

---
+++ Java/HashMapsJava.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class HashMapsJava {
+    public static void main(String[] args) {
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(1, "Kiran");
+        map.put(2, "Raj");
+        map.put(3, "Rajesh");
+    }
+    
+}
