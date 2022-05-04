612ebade2e21644d55ad3d87fc3f79a75d90a0b6
Kiran Deep <kirandeep102030@gmail.com>
2022-05-04 20:35:00 +0530

    remove() implemented in HM's🦍

--- Java/HashMapsJava.java
+++ Java/HashMapsJava.java
@@ -7,6 +7,8 @@ public class HashMapsJava {
         map.put(2, "Raj");
         map.put(3, "Rajesh");
         System.out.printf("%s\n%s\n%s\n", map.entrySet(), map.keySet(), map.values());
+        map.remove(1);
+        System.out.printf("After removing key 1 : %s\n", map.entrySet());
         for (Map.Entry<Integer, String> m : map.entrySet()) {
             System.out.println(m.getKey() + " " + m.getValue());
         }
