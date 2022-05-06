818e279b22be3742a20d0798bb80e5411ca3fc96
Kiran Deep <kirandeep102030@gmail.com>
2022-05-06 22:53:14 +0530

    Created new file and added basic code of collections

---
+++ Java/CollectionExplorer.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class CollectionExplorer {
+    public static void main(String[] args) {
+        Collection<String> collection = new ArrayList<>();
+        collection.add("one");
+        collection.add("two");
+        collection.add("three");
+        collection.add("four");
+        System.out.println(collection);
+    }
+}
