9f8964a362c7a246c87ce369f656767d62aee056
Kiran Deep <kirandeep102030@gmail.com>
2022-05-21 13:00:16 +0530

    Added LongestPalindrome

--- DS/Leetcode DSA/LongestPalindrome.java
+++ DS/Leetcode DSA/LongestPalindrome.java
@@ -3,6 +3,8 @@ import java.util.*;
 class LongestPalindrome {
     public static void main(String[] args) {
         String s;
+        int length=0;
+        boolean maxOddValue=false;
         Scanner in = new Scanner(System.in);
         s = in.next();
         HashMap<Character, Integer> hm = new HashMap<>();
@@ -13,9 +15,19 @@ class LongestPalindrome {
                 hm.put(i, 1);
         }
 
+        System.out.println(hm.toString());
+
         for (Map.Entry<Character, Integer> m: hm.entrySet()) {
-            System.out.println(m.getKey() + " " + m.getValue());
+            if(m.getValue()%2 == 0)
+                length += m.getValue();
+            else {
+                length += m.getValue() - 1;
+                maxOddValue = true;
+            }
         }
+        if(maxOddValue)
+            length++;
+        System.out.println(length);
         in.close();
     }
 }
