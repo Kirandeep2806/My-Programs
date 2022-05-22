2eff218a5618dff26c37c6e4cc37977956c13180
Kiran Deep <kirandeep102030@gmail.com>
2022-05-22 15:32:10 +0530

    Staging #2

--- DS/Leetcode DSA/AssignCookies.java
+++ DS/Leetcode DSA/AssignCookies.java
@@ -12,10 +12,21 @@ public class AssignCookies {
         inp = sc.nextLine();
         for (String i : inp.split(" "))
             s.add(Integer.parseInt(i));
-        System.out.println(g + " " + s);
+        // System.out.println(g + " " + s);
         g.sort((a, b) -> a.compareTo(b));
         s.sort((a, b) -> a.compareTo(b));
-        System.out.println(g + " " + s);
+        // System.out.println(g + " " + s);
+        int i = 0, j = 0 ,res = 0, maxG=g.size(), maxS=s.size();
+        while(i!=maxG && j!=maxS) {
+            if(s.get(j) >= g.get(i)) {
+                res++;
+                i++;
+                j++;
+            }
+            else
+                break;
+        }
+        System.out.println(res);
         sc.close();
     }
 }
