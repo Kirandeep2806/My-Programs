9726cb02291a29835af86bfbc21645f4848be93f
Kirandeep2806 <kirandeep102030@gmail.com>
2022-05-26 12:48:44 +0530

    Integer Boxing

--- Java/Explorer1.java
+++ Java/Explorer1.java
@@ -1,5 +1,15 @@
+import java.util.*;
+import java.util.stream.IntStream;
 public class Explorer1 {
     public static void main(String[] args) {
-        
+        int[] arr = {1,2,3,5,-3,4};
+        Integer[] boxedInts = IntStream.of(arr).boxed().toArray(Integer[]::new);
+        System.out.println(boxedInts.getClass());
+        System.out.println("Hi");
+        Arrays.parallelSort(arr);
+        // ArrayList<Integer> al = new ArrayList<>(Arrays.asList(arr));
+        // Collections.reverse();
+        // Arrays.fill(arr, 0);
+        System.out.println(Arrays.toString(arr));
     }
 }
