ab500722f328960761c1986a13cf21f01a072ec7
Kiran Deep <kirandeep102030@gmail.com>
2022-05-30 12:38:19 +0530

    Added String Permutations#1

--- DS/GFG/StringPermutation.py
+++ DS/GFG/StringPermutation.py
@@ -1,3 +1,12 @@
+#!/usr/bin/python3
+
+def permutations(string, l, r):
+    if l == r:
+        print(string)
+    for i in range(l, r):
+        string[l], string[i] = string[i], string[l]
+        permutations(string, l+1, r)
+
 srcString = "ABC"
-for i in srcString:
-    print(i)
+string = list(srcString)
+permutations(string, 0, len(string))
