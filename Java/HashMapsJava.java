2ce96072b4482c2c63d9bb6136009fa72a15e9a1
Kiran Deep <kirandeep102030@gmail.com>
2022-06-26 16:50:38 +0530

    Explored computeIfAbsent() and computeIfPresent()

--- Java/HashMapsJava.java
+++ Java/HashMapsJava.java
@@ -8,9 +8,12 @@ public class HashMapsJava {
         map.put(3, "Rajesh");
         System.out.printf("%s\n%s\n%s\n", map.entrySet(), map.keySet(), map.values());
         map.remove(1);
+        map.computeIfAbsent(4, k -> "Default"); // Set to default if key not present
+        map.computeIfPresent(4, (k, v) -> v + " Kiran"); // Update the value if it present
         System.out.printf("After removing key 1 : %s\n", map.entrySet());
         for (Map.Entry<Integer, String> m : map.entrySet()) {
             System.out.println(m.getKey() + " " + m.getValue());
         }
+        System.out.println();
     }
 }
