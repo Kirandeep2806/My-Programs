2eca957339f371f4842cd3966408847c063b331a
Kiran Deep <kirandeep102030@gmail.com>
2022-08-04 22:05:42 +0530

    Power set final commit

--- DS/GFG/powerSet.py
+++ DS/GFG/powerSet.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+# Pre-requisite : Brian Kerningam Algorithm
+
 from math import log2
 
 s = input("Enter a string : ")
@@ -7,8 +9,8 @@ n = len(s)
 
 for i in range(2**n):
     res = ''
-    while i>0:
-        lastSetBit = i&(~(i-1))
-        res += s[int(log2(lastSetBit))]
-        i = i&(i-1)
+    while i>0: # Ex : i=011, i=010
+        lastSetBit = i&(~(i-1)) # find the last set bit. => 001, 010
+        res += s[int(log2(lastSetBit))] # fetch the character@lastSetBit. => res = 'a', res='ab'
+        i = i&(i-1) # remove that particular last set bit and proceed until no bits are set. => i=010, i=000
     print(res)
