d7488c3e76edeb0c3f22b86d701564ca19d440c9
Kiran Deep <kirandeep102030@gmail.com>
2022-08-09 19:43:59 +0530

    Lucky Number(staging)

---
+++ DS/GFG/Recursion/LuckyNumbers.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class LuckyNumbers {
+    public static boolean isLucky(int n, int pos) {
+        if(pos > n)
+            return true;
+        if(n%pos == 0)
+            return false;
+        return isLucky(n-n/pos, pos+1);
+    }
+
+    public static void main(String[] args)
+    throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        // if(isLucky(n, 2))
+    }
+}
