d92c94ecf56b1e7226bc51bf12205ce22e78e734
Kiran Deep <kirandeep102030@gmail.com>
2022-08-09 20:56:15 +0530

    Change the params to default

--- DS/GFG/Recursion/LuckyNumbers.java
+++ DS/GFG/Recursion/LuckyNumbers.java
@@ -1,7 +1,10 @@
 import java.io.*;
 
 public class LuckyNumbers {
-    public static boolean isLucky(int n, int pos) {
+    public static boolean isLucky(int n, int... params) {
+        int pos = 2;
+        if(params.length != 0)
+            pos = params[0];
         if(pos > n)
             return true;
         if(n%pos == 0)
@@ -13,7 +16,7 @@ public class LuckyNumbers {
     throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        if(isLucky(n, 2))
+        if(isLucky(n))
             System.out.println("Lucky Number");
         else
             System.out.println("Not a lucky number");
