06471426cfb1abbd71d6dc8cf6558061dd22feeb
Kiran Deep <kirandeep102030@gmail.com>
2022-08-10 22:39:51 +0530

    Added POPULATION

---
+++ DS/CodeChef/START51D/POPULATION.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int x,y,z;
+        cin >> x >> y >> z;
+        cout << x-y+z << "\n";
+    }
+    return 0;
+}
