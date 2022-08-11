a889649d59b7b845083262098f0d96af53e12ec5
Kiran Deep <kirandeep102030@gmail.com>
2022-08-11 21:19:11 +0530

    Find the 2nd largest element staging

--- DS/GFG/Arrays/SecondLargestElement.java
+++ DS/GFG/Arrays/SecondLargestElement.java
@@ -3,6 +3,14 @@ import java.io.*;
 public class SecondLargestElement {
     public static void main(String[] args) 
     throws java.lang.Exception{
-        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inp = br.readLine().split(" ");
+        int[] intInput = new int[inp.length];
+        for(int i=0; i<inp.length; i++) 
+            intInput[i] = Integer.parseInt(inp[i]);
+        int largest=0, res=-1;
+        for (int i = 0; i < intInput.length; i++) {
+            
+        }
     }
 }
