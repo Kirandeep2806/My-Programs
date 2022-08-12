e7ee55dfcc1fc8a0b6c65d5cfb6d58ebc504387a
Kiran Deep <kirandeep102030@gmail.com>
2022-08-12 10:01:40 +0530

    Starter template for NonDuplicatesInSortedArray

---
+++ DS/GFG/Arrays/NonDuplicatesInSortedArray.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class NonDuplicatesInSortedArray {
+    public static void main(String[] args)
+    throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inp = br.readLine().split(" ");
+        int[] intInput = new int[inp.length];
+        for(int i=0; i<inp.length; i++) 
+            intInput[i] = Integer.parseInt(inp[i]);
+        int res = 0;
+        for (int i = 1; i < intInput.length; i++) {
+            
+        }
+    }
+}
--- DS/GFG/Arrays/SecondLargestElement.java
+++ DS/GFG/Arrays/SecondLargestElement.java
@@ -1,7 +1,7 @@
 import java.io.*;
 
 public class SecondLargestElement {
-    public static void main(String[] args) 
+    public static void main(String[] args)
     throws java.lang.Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] inp = br.readLine().split(" ");
