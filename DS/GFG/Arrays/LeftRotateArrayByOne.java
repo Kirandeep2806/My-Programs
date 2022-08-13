052faec88477993dd04d0c08f6ef699bc7eb8c09
Kiran Deep <kirandeep102030@gmail.com>
2022-08-13 19:33:27 +0530

    Added code for 'Left Rotate Array by one'

--- DS/GFG/Arrays/LeftRotateArrayByOne.java
+++ DS/GFG/Arrays/LeftRotateArrayByOne.java
@@ -9,6 +9,10 @@ public class LeftRotateArrayByOne {
         int[] arr = new int[inp.length];
         for(int i=0; i<arr.length; i++)
             arr[i] = Integer.parseInt(inp[i]);
-        
+        int temp = arr[0];
+        for (int i = 1; i < arr.length; i++)
+            arr[i-1] = arr[i];
+        arr[arr.length-1] = temp;
+        System.out.println(Arrays.toString(arr));
     }
 }
