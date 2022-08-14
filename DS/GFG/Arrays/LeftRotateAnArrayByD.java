a182a5f60a54974d25cf7b218fb36b3bc654478f
Kiran Deep <kirandeep102030@gmail.com>
2022-08-14 21:57:26 +0530

    Taken space separated inputs

---
+++ DS/GFG/Arrays/LeftRotateAnArrayByD.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class LeftRotateAnArrayByD {
+    public static void main(String[] args)
+    throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inp = br.readLine().split(" ");
+        int[] intInput = new int[inp.length];
+        for(int i=0; i<intInput.length; i++)
+            intInput[i] = Integer.parseInt(inp[i]);
+        
+    }
+}
