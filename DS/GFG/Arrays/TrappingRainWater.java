3662330213676f81f77ef37c00624221b20ff8e9
Kiran Deep <kirandeep102030@gmail.com>
2022-08-15 19:47:51 +0530

    TrappingRainWater@Staging

---
+++ DS/GFG/Arrays/TrappingRainWater.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class TrappingRainWater {
+    public static void main(String[] args)
+    throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inp = br.readLine().split(" ");
+        int[] arr = new int[inp.length];
+        for(int i=0; i<arr.length; i++)
+            arr[i] = Integer.parseInt(inp[i]);
+        
+    }
+}
