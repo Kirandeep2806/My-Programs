6a25e75529a93d8873efb8971f3ca4738f46f75e
Kiran Deep <kirandeep102030@gmail.com>
2022-08-15 19:45:59 +0530

    Added code for Buy and Sell Stocks

--- DS/GFG/Arrays/BuyAndSellStocks.java
+++ DS/GFG/Arrays/BuyAndSellStocks.java
@@ -9,5 +9,11 @@ public class BuyAndSellStocks {
         int[] arr = new int[inp.length];
         for(int i=0; i<arr.length; i++)
             arr[i] = Integer.parseInt(inp[i]);
+        int profit = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i] > arr[i-1])
+                profit += arr[i] - arr[i-1];
+        }
+        System.out.println(profit);
     }
 }
