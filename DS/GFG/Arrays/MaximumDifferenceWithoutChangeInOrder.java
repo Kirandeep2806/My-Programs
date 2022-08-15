e823c90bd38afcfde495eaa4b2dc91136a771a2f
Kiran Deep <kirandeep102030@gmail.com>
2022-08-15 18:05:04 +0530

    MaximumDifferenceWithoutChangeInOrder@Staging

---
+++ DS/GFG/Arrays/MaximumDifferenceWithoutChangeInOrder.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+public class MaximumDifferenceWithoutChangeInOrder {
+    public static void main(String[] args)
+    throws IOException {
+        
+    }
+}
