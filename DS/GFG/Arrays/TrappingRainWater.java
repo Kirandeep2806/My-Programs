52d898f5b7b027273d88ebec25fb32661026ff7b
Kiran Deep <kirandeep102030@gmail.com>
2022-08-15 20:00:13 +0530

    Added Trapping Rain Water

--- DS/GFG/Arrays/TrappingRainWater.java
+++ DS/GFG/Arrays/TrappingRainWater.java
@@ -9,14 +9,17 @@ public class TrappingRainWater {
         int[] arr = new int[inp.length];
         for(int i=0; i<arr.length; i++)
             arr[i] = Integer.parseInt(inp[i]);
-        int n = arr.length;
+        int n = arr.length, waterTrapped=0;
         int[] leftMaxArr = new int[n];
         int[] rightMaxArr = new int[n];
         leftMaxArr[0] = arr[0];
         rightMaxArr[n-1] = arr[n-1];
         for(int i=1; i<n; i++)
             leftMaxArr[i] = Math.max(arr[i], leftMaxArr[i-1]);
-        for(int i=n-2; i<=0; i--)
+        for(int i=n-2; i>=0; i--)
             rightMaxArr[i] = Math.max(arr[i], rightMaxArr[i+1]);
+        for(int i=1; i<n-1; i++)
+            waterTrapped += Math.min(leftMaxArr[i], rightMaxArr[i]) - arr[i];
+        System.out.println(waterTrapped);
     }
 }
