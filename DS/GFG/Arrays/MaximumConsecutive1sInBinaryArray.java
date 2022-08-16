e0267100cfc09b718546f150437a1003c7feb7a2
Kiran Deep <kirandeep102030@gmail.com>
2022-08-16 21:01:37 +0530

    Added Maximum Consecutive 1's In Binary Array

--- DS/GFG/Arrays/MaximumConsecutive1sInBinaryArray.java
+++ DS/GFG/Arrays/MaximumConsecutive1sInBinaryArray.java
@@ -7,8 +7,18 @@ public class MaximumConsecutive1sInBinaryArray {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] inp = br.readLine().split(" ");
         int[] arr = new int[inp.length];
-        for(int i=0; i<arr.length; i++)
+        int max_1s = Integer.MIN_VALUE;
+        int tempCounterFor1 = 0;
+        for(int i=0; i<arr.length; i++) {
             arr[i] = Integer.parseInt(inp[i]);
-        
+            if(arr[i] == 1)
+                tempCounterFor1++;
+            if(arr[i] == 0) {
+                max_1s = Integer.max(max_1s, tempCounterFor1);
+                tempCounterFor1 = 0;
+            }
+        }
+        max_1s = Integer.max(max_1s, tempCounterFor1);
+        System.out.println(max_1s);
     }
 }
