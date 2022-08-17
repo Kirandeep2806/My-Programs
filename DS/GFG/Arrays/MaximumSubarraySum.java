8d3ddb62744b83910864ffff4ac8d11b2b87ecc8
Kiran Deep <kirandeep102030@gmail.com>
2022-08-17 19:41:15 +0530

    Added Maximum Subarray Sum(Kadane's Algorithm/Java)

--- DS/GFG/Arrays/MaximumSubarraySum.java
+++ DS/GFG/Arrays/MaximumSubarraySum.java
@@ -1,9 +1,23 @@
 import java.io.*;
-import java.util.*;
 
 public class MaximumSubarraySum {
     public static void main(String[] args)
     throws IOException {
-        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inp = br.readLine().split(" ");
+        int n = inp.length;
+        int[] arr = new int[n];
+        int maxSum = Integer.MIN_VALUE;
+        int sumTillNow = 0;
+        for(int i=0; i<arr.length; i++) {
+            arr[i] = Integer.parseInt(inp[i]);
+            sumTillNow += arr[i];
+            if(sumTillNow < 0)
+                sumTillNow = 0;
+            else
+                maxSum = Math.max(sumTillNow, maxSum);
+        }
+        maxSum = Math.max(sumTillNow, maxSum); // To handle the case where all the inputs are -ve
+        System.out.println(maxSum);
     }
 }
