5dfb2becbb57430ce2f896383538a1e4dcd25a68
Kiran Deep <kirandeep102030@gmail.com>
2022-09-02 20:52:48 +0530

    Added Kadane's algorithm

--- DS/GFG/Arrays/MaximumCircularSubArray.java
+++ DS/GFG/Arrays/MaximumCircularSubArray.java
@@ -2,6 +2,19 @@ import java.io.*;
 import java.util.*;
 
 public class MaximumCircularSubArray {
+
+    public static int MaximumSubArray(int... arr) {
+        int maxSum = Integer.MIN_VALUE;
+        int tempSum = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if(tempSum < 0)
+                tempSum = 0;
+            tempSum += arr[i];
+            maxSum = Math.max(tempSum, maxSum);
+        }
+        return maxSum;
+    }
+
     public static void main(String[] args)
     throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -9,6 +22,7 @@ public class MaximumCircularSubArray {
         int[] arr = new int[inp.length];
         for(int i=0; i<arr.length; i++)
             arr[i] = Integer.parseInt(inp[i]);
-        
+        int res = MaximumSubArray(arr);
+        System.out.println(res);
     }
 }
