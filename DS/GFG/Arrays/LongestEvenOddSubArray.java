21259d96fa1bdf9b5c011caadcd0388bcefc2961
Kiran Deep <kirandeep102030@gmail.com>
2022-09-02 20:33:35 +0530

    Added Maximum Length Even-Odd Subarray

--- DS/GFG/Arrays/LongestEvenOddSubArray.java
+++ DS/GFG/Arrays/LongestEvenOddSubArray.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.*;
 
 public class LongestEvenOddSubArray {
     public static void main(String[] args)
@@ -9,6 +8,16 @@ public class LongestEvenOddSubArray {
         int[] arr = new int[inp.length];
         for(int i=0; i<arr.length; i++)
             arr[i] = Integer.parseInt(inp[i]);
-        
+        int longestOddEvenSubArray = 1;
+        int tentativeLongestOddEvenSubArray = 1;
+        for (int i = 1; i < arr.length; i++) {
+            if((arr[i]%2==0 && arr[i-1]%2!=0) || (arr[i]%2==1 && arr[i-1]%2==0)) {
+                tentativeLongestOddEvenSubArray++;
+                longestOddEvenSubArray = Math.max(longestOddEvenSubArray, tentativeLongestOddEvenSubArray);
+            }
+            else
+                tentativeLongestOddEvenSubArray = 1;
+        }
+        System.out.println(longestOddEvenSubArray);
     }
 }
