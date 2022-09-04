99b3ca2809fbaa11e25f21b9fd5b33cf53ef308c
Kiran Deep <kirandeep102030@gmail.com>
2022-09-04 15:09:56 +0530

    Added code for Subarray with given sum

--- DS/GFG/Arrays/SubarrayWithGivenSum.py
+++ DS/GFG/Arrays/SubarrayWithGivenSum.py
@@ -1,3 +1,29 @@
+#!/usr/bin/python3
+
 arr = list(map(int, input().split()))
+targetSum = int(input())
 
+ptr = 0
+n = len(arr)
+currSum = 0
+windowSize = 0
+found = False
 
+while ptr<=n:
+    if currSum < targetSum:
+        if ptr >= n:
+            break
+        currSum += arr[ptr]
+        windowSize += 1
+        ptr += 1
+    elif currSum > targetSum:
+        currSum -= arr[ptr-windowSize]
+        windowSize -= 1
+    else:
+        found = True
+        break
+
+if found:
+    print("Yes :", windowSize)
+else:
+    print("No")
