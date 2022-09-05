7a481f8f91ac636c1a9e7b7dc23cfa8100cdea09
Kiran Deep <kirandeep102030@gmail.com>
2022-09-05 23:58:55 +0530

    Added SetMatrixZeroes

--- DS/Leetcode DSA/SetMatrixZeroes.py
+++ DS/Leetcode DSA/SetMatrixZeroes.py
@@ -1,3 +1,22 @@
 #!/usr/bin/python3
 
+matrix = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
+# matrix = [[0, 1, 2, 0], [3, 4, 5, 2], [1, 3, 1, 5]]
+m = matrix.__len__()
+n = matrix[0].__len__()
 
+x = {}
+y = {}
+
+for i in range(m):
+    for j in range(n):
+        if matrix[i][j] == 0:
+            x[i] = True
+            y[i] = True
+
+for i in range(m):
+    for j in range(n):
+        if i in x or j in y:
+            matrix[i][j] = 0
+
+print(matrix)
