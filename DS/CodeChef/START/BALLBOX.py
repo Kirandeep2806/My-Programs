e1c34538b16728ee2935049dd63cada137d5a68e
Kiran Deep <kirandeep102030@gmail.com>
2022-09-07 21:55:10 +0530

    Folder Rename

--- DS/CodeChef/START/BALLBOX.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-for i in range(int(input())):
-    n, k = list(map(int, input().split()))
-    if n >= ((k*(k+1))//2):
-        print("YES")
-    else:
-        print("NO")
--- DS/CodeChef/START/FEVER.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-t = int(input())
-for i in range(t):
-    n = int(input())
-    if n > 98:
-        print("YES")
-    else:
-        print("NO")
