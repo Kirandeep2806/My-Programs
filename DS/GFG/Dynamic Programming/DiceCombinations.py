7515a676d11ee748f1286aeae8277cbb1d4e43c7
Kiran Deep <kirandeep102030@gmail.com>
2022-09-08 20:18:20 +0530

    DiceCombinations@Staging

--- DS/GFG/Dynamic Programming/DiceCombinations.py
+++ DS/GFG/Dynamic Programming/DiceCombinations.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 
-def diceCombinations() :
-    pass
+def diceCombinations(n, res, mem={}):
+    if n == 0:
+        return 0
+    if mem.get(n, 0):
+        return res+mem[n]
+    possibleWays = 0
+    for i in range(1, n+1):
+        possibleWays += 1
+        diceCombinations(n-i)
+    mem[n] = possibleWays
+    return res+possibleWays
 
 n = int(input())
-res = diceCombinations(n)
+res = diceCombinations(n, 0)
 print(res)
