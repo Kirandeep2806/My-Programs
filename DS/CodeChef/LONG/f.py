954d30e287e81425fc056fb6a30b1271a81f13b3
Kiran Deep <kirandeep102030@gmail.com>
2022-09-09 23:32:34 +0530

    f@Staging

--- DS/CodeChef/LONG/f.py
+++ DS/CodeChef/LONG/f.py
@@ -1,25 +1,50 @@
 #!/usr/bin/python3
 
+# from collections import OrderedDict
+
+from logging import getLogRecordFactory
+
+
 for i in range(int(input())):
     n = int(input())
     strengthOnGround = list(map(int, input().split()))
     strengthOnWater = list(map(int, input().split()))
-    # sortedGround = sorted(list(enumerate(strengthOnGround)), key=lambda x:x[1], reverse=True)
-    # sortedWater = sorted(list(enumerate(strengthOnWater)), key=lambda x:x[1], reverse=True)
-    l1 = set()
-    l2 = set()
-    lg = [0]*n
-    lw = lg.copy()
-    res = 0
-    maxStrength = 0
+    strengthOnGround = [i[0] for i in sorted(list(enumerate(strengthOnGround)), key=lambda x:x[1], reverse=True)]
+    strengthOnWater = [i[0] for i in sorted(list(enumerate(strengthOnWater)), key=lambda x:x[1], reverse=True)]
+    # print(strengthOnGround, strengthOnWater)
+    dWater = {}
+    dGround = {}
+    for index, val in enumerate(strengthOnWater):
+        dWater[val] = index
+    for index, val in enumerate(strengthOnGround):
+        dGround[val] = index
+    print(strengthOnGround, strengthOnWater)
+    print(dGround, dWater)
+    # 0 - consider ground
+    # 1 - consider water
+    maxVal = 0
+    maxCount = 0
     for i in range(n):
-        pass
-        # lg[sortedGround[i][0]] += n-i-1
-        # maxStrength = max(maxStrength, lg[sortedGround[i][0]])
-    # print(sortedGround)
-    # print(lg)
-    for i in range(n):
-        pass
-        # lw[sortedWater[i][0]] += n-i-1
-    # print(sortedWater)
-    print(lg)
+        res = 0
+        # considerIteration = 0
+        # if dGround[strengthOnGround[i]] < dWater[strengthOnWater[i]]:
+        #     considerIteration = 1
+        # if considerIteration == 1:
+        # considerNumbers = n - dGround[strengthOnGround[i]] - 1
+        # for j in range(dWater[strengthOnWater[i]], n):
+        #     if dGround.get(strengthOnWater[j], -1) != -1:
+        #         considerNumbers -= 1
+        #     res += 1
+        # else:
+        considerNumbers = n - dWater[strengthOnWater[i]] - 1
+        for j in range(dGround[strengthOnGround[i]]+1, n):
+            if dWater.get(strengthOnGround[j], -1) != -1:
+                considerNumbers -= 1
+            res += 1
+        res += considerNumbers
+        if res > maxVal:
+            maxVal = res
+            maxCount = 1
+        elif res == maxVal:
+            maxCount += 1
+    print(maxCount)
