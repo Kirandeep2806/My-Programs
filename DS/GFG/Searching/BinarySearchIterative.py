646249efc99afc7c0d7b0e2ca67736da45b143e3
Kiran Deep <kirandeep102030@gmail.com>
2022-09-11 08:58:24 +0530

    Added Binary Search Code(Iterative)

---
+++ DS/GFG/Searching/BinarySearchIterative.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def binarySearch(arr, target, low, high):
+    while low <= high:
+        mid = (low+high)//2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid+1
+        else:
+            high = mid-1
+    else:
+        return -1
+
+arr = list(map(int, input().split()))
+target = int(input())
+searchRes = binarySearch(arr, target, 0, len(arr)-1)
+print(searchRes)
