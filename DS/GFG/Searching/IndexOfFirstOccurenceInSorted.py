b1882ea75bf63d83cbf65dc52f9c58dd0a8512df
Kiran Deep <kirandeep102030@gmail.com>
2022-09-11 09:05:51 +0530

    Added IndexOfFirstOccurenceInSorted

--- DS/GFG/Searching/IndexOfFirstOccurenceInSorted.py
+++ DS/GFG/Searching/IndexOfFirstOccurenceInSorted.py
@@ -1,3 +1,20 @@
 #!/usr/bin/python3
 
+def binarySearch(arr, target, low, high):
+    while low <= high:
+        mid = (low+high)//2
+        if arr[mid] == target and mid == 0:
+            return mid
+        elif arr[mid] == target and arr[mid] != arr[mid-1]:
+            return mid
+        elif arr[mid] < target:
+            low = mid+1
+        else:
+            high = mid-1
+    else:
+        return -1
 
+arr = list(map(int, input() or "1 1 10 10 10 20 20 40".split()))
+target = int(input())
+res = binarySearch(arr, target, 0, len(arr)-1)
+print(res)
