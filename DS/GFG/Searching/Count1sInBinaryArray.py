9fe7657c8d21b82b74783d45c51b81309ec29133
Kiran Deep <kirandeep102030@gmail.com>
2022-09-11 18:13:50 +0530

    Added Count1sInBinaryArray

--- DS/GFG/Searching/Count1sInBinaryArray.py
+++ DS/GFG/Searching/Count1sInBinaryArray.py
@@ -1,3 +1,20 @@
 #!/usr/bin/python3
 
+def Count1sInBinaryArray(arr, n):
+    low = 0
+    high = n-1
+    while low <= high:
+        mid = (low+high)//2
+        if arr[mid] == 0:
+            low = mid+1
+        elif mid!=0 and arr[mid-1]==arr[mid]:
+            high = mid-1
+        elif arr[mid] == 1:
+            return n-mid
+    else:
+        return 0
 
+
+arr = list(map(int, (input() or "0 0 1 1 1 1 1").split()))
+res = Count1sInBinaryArray(arr, len(arr))
+print(res)
