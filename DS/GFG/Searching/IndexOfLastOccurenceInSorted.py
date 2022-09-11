5a4e4cecd723556ba9272b455efe0065c20299fa
Kiran Deep <kirandeep102030@gmail.com>
2022-09-11 09:42:58 +0530

    Added IndexOfLastOccurenceInSorted

--- DS/GFG/Searching/IndexOfLastOccurenceInSorted.py
+++ DS/GFG/Searching/IndexOfLastOccurenceInSorted.py
@@ -3,18 +3,18 @@
 def binarySearch(arr, target, low, high):
     while low <= high:
         mid = (low+high)//2
-        if arr[mid] == target and mid == 0:
+        if arr[mid] == target and mid == len(arr)-1:
             return mid
-        elif arr[mid] == target and arr[mid] != arr[mid-1]:
+        elif arr[mid] == target and arr[mid] != arr[mid+1]:
             return mid
-        elif arr[mid] < target:
-            low = mid+1
-        else:
+        elif arr[mid] > target:
             high = mid-1
+        else:
+            low = mid+1
     else:
         return -1
 
-arr = list(map(int, (input() or "1 1 10 10 10 20 20 40".split())))
+arr = list(map(int, (input() or "1 1 10 10 10 20 20 40").split()))
 target = int(input())
 res = binarySearch(arr, target, 0, len(arr)-1)
 print(res)
