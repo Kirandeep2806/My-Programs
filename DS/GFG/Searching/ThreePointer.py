de22925587150f5da42498f7e5edf98ddb3053a0
Kiran Deep <kirandeep102030@gmail.com>
2022-09-14 00:43:17 +0530

    Added 3Pointer

--- DS/GFG/Searching/ThreePointer.py
+++ DS/GFG/Searching/ThreePointer.py
@@ -1,7 +1,28 @@
 #!/usr/bin/python3
 
+from BinarySearchIterative import binarySearch
+
 arr = list(map(int, input().split()))
 targetSum = int(input())
 arr.sort()
 
+ptr1 = 0
+ptr2 = len(arr)-1
 
+while ptr1<ptr2:
+    twoSum = arr[ptr1] + arr[ptr2]
+    requiredVal = targetSum - twoSum
+    if arr[ptr1] <= requiredVal <= arr[ptr2]:
+        res = binarySearch(arr, requiredVal, ptr1+1, ptr2-1)
+        if res!=-1:
+            print(arr[ptr1], arr[ptr2], arr[res])
+            break
+    if twoSum > targetSum:
+        ptr2 -= 1
+    elif twoSum < targetSum:
+        ptr1 += 1
+    else:
+        print("-1")
+        break
+else:
+    print("-1")
