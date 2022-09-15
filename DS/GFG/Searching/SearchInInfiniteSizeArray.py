d16d031d6cf255007005f376a580bcf4c2b3092f
Kiran Deep <kirandeep102030@gmail.com>
2022-09-15 22:51:54 +0530

    Added SearchInfiniteSizeArray

--- DS/GFG/Searching/SearchInInfiniteSizeArray.py
+++ DS/GFG/Searching/SearchInInfiniteSizeArray.py
@@ -4,6 +4,20 @@ from BinarySearchIterative import binarySearch
 
 arr = list(map(int, input().split())) # Input should be a sorted array
 target = int(input())
-n = 1
-while True:
-    pass
+i = 1
+res = 0
+if arr[res] == target:
+    print(res)
+else:
+    while True:
+        if arr[i] == target:
+            res = i
+            break
+        elif arr[i] > target:
+            res = binarySearch(arr, target, i//2+1, i-1)
+            break
+        i <<= 1
+    if res != -1:
+        print(res)
+    else:
+        print("Not Found")
