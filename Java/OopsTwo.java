b45a0d2878d43f4f0784b63d280d21cc36f66077
Kiran Deep <kirandeep102030@gmail.com>
2022-09-20 22:10:46 +0530

    OopsTwo@Staging

---
+++ Java/OopsTwo.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+class Person {
+    String name;
+    int age;
+    public Person(String name, int age ) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+
+public class OopsTwo {
+    public static void main(String[] args)
+    throws IOException {
+        
+    }
+}
