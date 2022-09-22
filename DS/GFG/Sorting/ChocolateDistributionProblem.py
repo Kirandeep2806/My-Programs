ddfeb1fee13038ed16301c554fcf877242e0cacc
Kiran Deep <kirandeep102030@gmail.com>
2022-09-22 23:12:40 +0530

    Added ChocolateDistributionProblem

--- DS/GFG/Sorting/ChocolateDistributionProblem.py
+++ DS/GFG/Sorting/ChocolateDistributionProblem.py
@@ -1,3 +1,20 @@
 #!/usr/bin/python3
 
+from sys import maxsize
+
 arr = list(map(int, input().split()))
+m = int(input())
+
+arr.sort()
+minVal = maxsize
+startIndex = 0
+
+for i in range(arr.__len__()-m+1):
+    diff = arr[i+m-1] - arr[i]
+    if diff < minVal:
+        minVal = diff
+        startIndex = i
+
+# print(minVal, startIndex)
+print(arr[startIndex:startIndex+m], minVal)
+
