02cd2f163399d6a59e62c42a905146cf019d2533
Kiran Deep <kirandeep102030@gmail.com>
2022-09-26 22:32:00 +0530

    MergeOverlappingIntervals@Staging

--- DS/GFG/Sorting/MergeOverlappingIntervals.py
+++ DS/GFG/Sorting/MergeOverlappingIntervals.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
 
-arr = [[1,3],[2,4],[5,7],[6,8]]
+from itertools import count
 
+
+arr = [[1,3],[2,4],[5,7],[6,8]]
+a = [i[0] for i in arr]
+b = [i[1] for i in arr]
+
+a.sort()
+b.sort()
+
+printA = True
+count = 0
+
+while count<arr.__len__()*2:
+    pass
