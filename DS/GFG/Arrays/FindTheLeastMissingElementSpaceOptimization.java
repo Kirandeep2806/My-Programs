e2a4f16414541b3c61226e41f21cc68ed1ecc813
Kiran Deep <kirandeep102030@gmail.com>
2022-09-27 22:22:57 +0530

    FindTheLeastMissingElementSpaceOptimization@Staging

--- DS/GFG/Arrays/FindTheLeastMissingElementSpaceOptimization.java
+++ DS/GFG/Arrays/FindTheLeastMissingElementSpaceOptimization.java
@@ -2,8 +2,25 @@ import java.io.*;
 import java.util.*;
 
 public class FindTheLeastMissingElementSpaceOptimization {
+    public static int[] swap(int[] arr, int pos) {
+        arr[pos] = arr[pos]^arr[arr[pos]];
+        arr[arr[pos]] = arr[pos]^arr[arr[pos]];
+        arr[pos] = arr[pos]^arr[arr[pos]];
+        return arr;
+    }
+
     public static void main(String[] args)
     throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inp = br.readLine().split(" ");
+        int[] arr = new int[inp.length];
+        for(int i=0; i<arr.length; i++)
+            arr[i] = Integer.parseInt(inp[i]);
+        int n = arr.length, i = 0;
+        for(;i<n;i++) {
+            if(arr[i]!=i && arr[i]<n)
+                swap(arr, i);
+        }
         
     }
 }
