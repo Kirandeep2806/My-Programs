e92632f948543ea69397083ae2c8180c89e85331
Kiran Deep <kirandeep102030@gmail.com>
2022-10-03 16:35:39 +0530

    SpiralMatrix@Staging

--- DS/GFG/Matrix/SpiralMatrix.py
+++ DS/GFG/Matrix/SpiralMatrix.py
@@ -1,3 +1,48 @@
 #!/usr/bin/python3
 
+# l = [[1,2,3], [4,5,6], [7,8,9]]
+l = [[1,2,3,4],[5,6,7,8], [9,10,11,12]]
+# l = [[1,2,3,4]]
+r = l.__len__()
+c = l[0].__len__()
+
+top = 0
+right = c-1
+bottom =  r-1
+left = 0
+
+# top and bottom handles rows
+# left and right handles columns
+
+# 1 2 3
+# 4 5 6
+# 7 8 9
+
+# 1 2 3 4
+# 5 6 7 8
+# 9 10 11 12
+
+while(top<=bottom and left<=right):
+    tempTop = left
+    while(tempTop <= right):
+        print(l[top][tempTop], end=" ")
+        tempTop += 1
+    top += 1
+    tempRight = top
+    while(tempRight <= bottom):
+        print(l[tempRight][right], end=" ")
+        tempRight += 1
+    right -= 1
+    tempBottom = right
+    while(tempBottom >= left):
+        print(l[bottom][tempBottom], end=" ")
+        tempBottom -= 1
+    bottom -= 1
+    tempLeft = bottom
+    while(tempLeft >= top):
+        print(l[tempLeft][left], end=" ")
+        tempLeft -= 1
+    left += 1
+    print()
+
 
