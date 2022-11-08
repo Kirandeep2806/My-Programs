69402a493bad997f42f88fc3bae331b21bf9f098
Kiran Deep <kirandeep102030@gmail.com>
2022-11-08 22:11:19 +0530

    Added Balanced Binary Tree

--- DS/Leetcode DSA/Trees/BalancedBinaryTree.py
+++ DS/Leetcode DSA/Trees/BalancedBinaryTree.py
@@ -1,26 +1,19 @@
-#!/usr/bin/python3
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-	def fetchHeight(self, root) -> int:
-		if root is None:
-			return 0
-		lh = 1+fetchHeight(root.left)
-		rh = 1+fetchHeight(root.right)
-		if abs(lh-rh)<=1:
-			return True
-		return False
 
+class Solution:
+    def computeHeight(self, root):
+        if root is None:
+            return 0
+        lh = self.computeHeight(root.left)
+        rh = self.computeHeight(root.right)
+        if abs(lh-rh)>1 or (lh==-1) or (rh==-1):
+            return -1
+        return 1+max(lh, rh)
+    
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-    	if root is None:
-			return 0
-		lh = 1+self.isBalanced(root.left)
-		rh = 1+self.isBalanced(root.right)
-		if abs(lh-rh)<=1:
-			return True
-		return False
+        return True if self.computeHeight(root)>-1 else False
