56445ad8a92d931726ddeb905d45f9e73df016f9
Kiran Deep <kirandeep102030@gmail.com>
2022-11-12 00:11:34 +0530

    BinaryTreeZigzagLevelOrderTraversal@Staging

---
+++ DS/Leetcode DSA/Trees/BinaryTreeZigzagLevelOrderTraversal.py
@@ -0,0 +1,12 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from collections import deque
+
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        pass
