cb8b66865c8479b45d7abc76446428b464c53802
Kiran Deep <kirandeep102030@gmail.com>
2022-11-12 17:23:35 +0530

    Added BinaryTreeZigzagLevelOrderTraversal

--- DS/Leetcode DSA/Trees/BinaryTreeZigzagLevelOrderTraversal.py
+++ DS/Leetcode DSA/Trees/BinaryTreeZigzagLevelOrderTraversal.py
@@ -8,5 +8,30 @@
 from collections import deque
 
 class Solution:
+    def __init__(self):
+        self.LtoR = True
+        self.res = []
+
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        pass
+        if root:
+            d = deque()
+            d.append(root)
+            while d.__len__():
+                temp = d.copy()
+                aux = deque()
+                levelRes = deque()
+                while temp.__len__()!=0:
+                    ele = temp.popleft()
+                    if self.LtoR:
+                        levelRes.append(ele.val)
+                    else:
+                        levelRes.appendleft(ele.val)
+                    if ele.left is not None:
+                        aux.append(ele.left)
+                    if ele.right is not None:
+                        aux.append(ele.right)
+                self.res.append(levelRes)
+                d = aux
+                self.LtoR = not self.LtoR
+        return self.res
+
