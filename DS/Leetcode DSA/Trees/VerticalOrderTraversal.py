9fc4d770693cad41d79d1e47241aee3a81bc12fb
Kiran Deep <kirandeep102030@gmail.com>
2022-11-21 23:11:11 +0530

    VerticalOrderTraversal@Staging

--- DS/Leetcode DSA/Trees/VerticalOrderTraversal.py
+++ DS/Leetcode DSA/Trees/VerticalOrderTraversal.py
@@ -1,3 +1,34 @@
 #!/usr/bin/python3
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
+from collections import defaultdict
+from sys import maxsize
+
+class Solution:
+	def __init__(self):
+		self.minVal = maxsize
+		self.maxVal = -maxsize-1
+		self.ds = [] # Format : (level, y, val)
+		self.res = defaultdict(list) # Contains the value to be returned
+	def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
+		self.ds.append([0, 0, root])
+		while self.ds:
+			packedData = self.ds.pop(0)
+			level, y, node = packedData
+			if node.left:
+				self.ds.append([level+1, y-1, node.left])
+			if node.right:
+				self.ds.append([level+1, y+1, node.right])
+			self.res[y].append(node.val)
+			self.minVal = min(self.minVal, y)
+			self.maxVal = max(self.maxVal, y)
+		self.ds.clear()
+		for i in range(self.minVal, self.maxVal+1):
+			self.ds.append(sorted(self.res[i]))
+		return self.ds
