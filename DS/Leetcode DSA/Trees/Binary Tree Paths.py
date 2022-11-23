c8d955de8dce37263ffcd926621ea0d5f9ba959c
Kiran Deep <kirandeep102030@gmail.com>
2022-11-24 04:14:34 +0530

    Added Description

--- DS/Leetcode DSA/Trees/Binary Tree Paths.py
+++ DS/Leetcode DSA/Trees/Binary Tree Paths.py
@@ -4,6 +4,12 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+
+# Given the root of a binary tree, return all root-to-leaf paths in any order.
+# A leaf is a node with no children.
+
+
 class Solution:
     def __init__(self):
         self.paths = []
