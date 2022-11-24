1dee1dedee792e9653c2ff15f1b508197e791cc0
Kiran Deep <kirandeep102030@gmail.com>
2022-11-24 05:45:41 +0530

    LCA@Staging

---
+++ DS/Leetcode DSA/Trees/LowestCommonAncestor.py
@@ -0,0 +1,116 @@
+#!/usr/bin/python3
+
+#User function Template for python3
+
+'''
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+'''
+class Solution:
+    def __init__(self):
+        self.h1 = []
+        self.h2 = []
+
+    def findHeight(self, root, target, height):
+        if root:
+            if root.data==target:
+                return height
+            currHeight = self.findHeight(root.left, target, height+1)
+            if currHeight!=None:
+                return currHeight
+            currHeight = self.findHeight(root.right, target, height+1)
+            if currHeight!=None:
+                return currHeight
+
+
+    def LCA(self, root, a, b):
+        self.h1 = self.findHeight(root, a, 1)
+        self.h2 = self.findHeight(root, b, 1)
+        print(self.h1, self.h2)
+
+
+import sys
+sys.setrecursionlimit(100000)
+#Contributed by Sudarshan Sharma
+from collections import deque
+# Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+# Function to Build Tree   
+def buildTree(s):
+    #Corner Case
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+        
+    # Creating list of strings from input 
+    # string after spliting by space
+    ip=list(map(str,s.split()))
+    
+    # Create the root of the tree
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+    
+    # Push the root to the queue
+    q.append(root)
+    size=size+1
+    
+    # Starting from the second element
+    i=1                                       
+    while(size>0 and i<len(ip)):
+        # Get and remove the front of the queue
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+        
+        # Get the current node's value from the string
+        currVal=ip[i]
+        
+        # If the left child is not null
+        if(currVal!="N"):
+            
+            # Create the left child for the current node
+            currNode.left=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.left)
+            size=size+1
+        # For the right child
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        # If the right child is not null
+        if(currVal!="N"):
+            
+            # Create the right child for the current node
+            currNode.right=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+    
+    
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(0,t):
+        s=input()
+        root=buildTree(s)
+        obj = Solution()
+
+        # Put your function here
+        arr = list(map(int, input().split()))
+        res = obj.LCA(root, arr[0], arr[1])
+        for i in res:
+            print (i, end = " ")
+        print('')
