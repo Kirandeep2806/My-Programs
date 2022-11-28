31f4c4c393220b155ada768a19e4271c3e8a4520
Kiran Deep <kirandeep102030@gmail.com>
2022-11-28 19:50:10 +0530

    Also added efficient approach for FindPlayersWithZeroOrOneLosses

--- DS/Leetcode DSA/POTD/Find Players With Zero or One Losses.cpp
+++ DS/Leetcode DSA/POTD/Find Players With Zero or One Losses.cpp
@@ -24,4 +24,29 @@ class Solution {
 };
 
 
+// Efficient Approach
 
+static vector<vector<int>> findWinners(const vector<vector<int>>& matches) noexcept {
+    array<bool, 100001> played = {};
+    array<int, 100001> losses = {};
+    
+    for (const vector<int>& match : matches) {
+        played[match[0]] = true;
+        played[match[1]] = true;
+        ++losses[match[1]];
+    }
+    
+    vector<vector<int>> ans(2);
+
+    for (int i = 0; i < size(played); ++i) {
+        if (played[i]) {
+            if (losses[i] == 0) {
+                ans[0].push_back(i);
+            } else if (losses[i] == 1) {
+                ans[1].push_back(i);
+            }
+        }
+    }
+
+    return ans;
+}
