b8675ca16ebd1925e7adf8d1bc88b47f45c86b40
Kiran Deep <kirandeep102030@gmail.com>
2022-11-30 01:41:51 +0530

    Added LCA Program

--- DS/Leetcode DSA/Trees/LowestCommonAncestor.py
+++ DS/Leetcode DSA/Trees/LowestCommonAncestor.py
@@ -1,102 +1,13 @@
-#!/usr/bin/python3
-
-#User function Template for python3
-
-'''
-class Node:
-    def __init__(self, val):
-        self.right = None
-        self.data = val
-        self.left = None
-'''
 class Solution:
-    def __init__(self):
-        self.h1 = []
-        self.h2 = []
-
-    def LCA(self, root, a, b):
-        pass
-
-
-import sys
-sys.setrecursionlimit(100000)
-#Contributed by Sudarshan Sharma
-from collections import deque
-# Tree Node
-class Node:
-    def __init__(self, val):
-        self.right = None
-        self.data = val
-        self.left = None
-
-# Function to Build Tree   
-def buildTree(s):
-    #Corner Case
-    if(len(s)==0 or s[0]=="N"):           
-        return None
-        
-    # Creating list of strings from input 
-    # string after spliting by space
-    ip=list(map(str,s.split()))
-    
-    # Create the root of the tree
-    root=Node(int(ip[0]))                     
-    size=0
-    q=deque()
-    
-    # Push the root to the queue
-    q.append(root)
-    size=size+1
-    
-    # Starting from the second element
-    i=1                                       
-    while(size>0 and i<len(ip)):
-        # Get and remove the front of the queue
-        currNode=q[0]
-        q.popleft()
-        size=size-1
-        
-        # Get the current node's value from the string
-        currVal=ip[i]
-        
-        # If the left child is not null
-        if(currVal!="N"):
-            
-            # Create the left child for the current node
-            currNode.left=Node(int(currVal))
-            
-            # Push it to the queue
-            q.append(currNode.left)
-            size=size+1
-        # For the right child
-        i=i+1
-        if(i>=len(ip)):
-            break
-        currVal=ip[i]
-        
-        # If the right child is not null
-        if(currVal!="N"):
-            
-            # Create the right child for the current node
-            currNode.right=Node(int(currVal))
-            
-            # Push it to the queue
-            q.append(currNode.right)
-            size=size+1
-        i=i+1
-    return root
-    
-    
-if __name__=="__main__":
-    t=int(input())
-    for _ in range(0,t):
-        s=input()
-        root=buildTree(s)
-        obj = Solution()
-
-        # Put your function here
-        arr = list(map(int, input().split()))
-        res = obj.LCA(root, arr[0], arr[1])
-        for i in res:
-            print (i, end = " ")
-        print('')
+    def lowestCommonAncestor(self, root: 'TreeNode', a: 'TreeNode', b: 'TreeNode') -> 'TreeNode':
+        if (root is None) or (root==a) or (root==b):
+            return root
+        val1=self.lowestCommonAncestor(root.left,a,b)
+        val2=self.lowestCommonAncestor(root.right,a,b)
+        if((val1==a and val2==b) or (val1==b and val2==a)):
+            return root
+        # elif(val1==a or val1==b): This condition is not valid when a root value is returned
+        elif(val1!=None):
+            return val1
+        elif(val2!=None):
+            return val2
