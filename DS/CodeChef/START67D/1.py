e4121f3475f6fa42c006df2e0bcbf9fc8c2d2bc0
Kiran Deep <kirandeep102030@gmail.com>
2022-11-30 19:48:50 +0530

    Starters 67@Staging

---
+++ DS/CodeChef/START67D/1.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+for _ in range(int(input())):
+	n=int(input())
+	l=list(map(int,input().split()))
+	
---
+++ DS/CodeChef/START67D/4.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+for _ in range(int(input())):
+	n=int(input())
+	l=list(map(int,input().split()))
+	
+#!/usr/bin/python3
+
+for _ in range(int(input())):
+	n=int(input())
+	l=list(map(int,input().split()))
+	
