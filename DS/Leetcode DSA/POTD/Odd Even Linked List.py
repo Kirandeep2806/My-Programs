096e42597246b74e0868d1d5ed186fac96938398
Kiran Deep <kirandeep102030@gmail.com>
2022-12-16 22:24:22 +0530

    OddEvenLinkedList@staging

---
+++ DS/Leetcode DSA/POTD/Odd Even Linked List.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    	start=head
+    	count=1
+    	ptr=head
+    	while start!=None:
+    		pass
