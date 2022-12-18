c656f6b9d55ef9f5306a67a15dd14d0923dc52fa
Kiran Deep <kirandeep102030@gmail.com>
2022-12-18 07:48:02 +0530

    Added DailyTemperature

--- DS/Leetcode DSA/POTD/Daily Temperatures.py
+++ DS/Leetcode DSA/POTD/Daily Temperatures.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3
 
+from typing import List
 
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+    	n=len(temperatures)
+    	res=[0]*n
+    	s=[]
+    	for i,v in enumerate(temperatures):
+    		while s and s[-1][1]<v:
+			    popIndex=s.pop()[0]
+			    res[popIndex]=i-popIndex
+    		s.append([i,v])
+    	return res
+
+temperatures=[73,74,75,71,69,72,76,73]
+print(Solution().dailyTemperatures(temperatures))
