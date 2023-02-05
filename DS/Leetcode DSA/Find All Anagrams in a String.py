a53b0423617d40a36497f41367ec50b47ba084d2
Kiran Deep <kirandeep102030@gmail.com>
2023-02-05 21:55:43 +0530

    Added Find All Anagrams in a String

--- DS/Leetcode DSA/Find All Anagrams in a String.py
+++ DS/Leetcode DSA/Find All Anagrams in a String.py
@@ -10,28 +10,28 @@ class Solution:
         if d[ele]==0:
             del d[ele]
 
-
-    def checkInclusion(self, s1: str, s2: str) -> bool:
+    def findAnagrams(self, s2: str, s1: str) -> bool:
+        l=[]
         n=len(s1)
         m=len(s2)
         if n>m:
-            return False
+            return []
         d1,d2={},{}
         for i in range(n):
             self.addElement(s1[i], d1)
             self.addElement(s2[i], d2)
         for i in range(i+1,m):
             if d1==d2:
-                return True
+                l.append(i-n)
             self.addElement(s2[i], d2)
             self.delElement(s2[i-n], d2)
         else:
             if d1==d2:
-                return True
-        return False
+                l.append(m-n)
+        return l
 
 
 s1=input()
 s2=input()
-s = Solution().checkInclusion(s1, s2)
+s = Solution().findAnagrams(s1, s2)
 print(s)
