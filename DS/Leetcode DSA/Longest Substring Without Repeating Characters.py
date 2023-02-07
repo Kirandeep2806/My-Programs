009daefb7e58e2588fd86e8c212c382eb9f1e5b1
Kiran Deep <kirandeep102030@gmail.com>
2023-02-08 00:20:14 +0530

    Added Longest Substring Without Repeating Characters

--- DS/Leetcode DSA/Longest Substring Without Repeating Characters.py
+++ DS/Leetcode DSA/Longest Substring Without Repeating Characters.py
@@ -4,10 +4,10 @@ from collections import defaultdict
 class Solution:
 	def lengthOfLongestSubstring(self, s: str) -> int:
 		n=len(s)
-		d=defaultdict(lambda:-1)
+		d=defaultdict(lambda:-1) #imp
 		res=i=0
 		for j in range(n):
-			i=max(i, d[s[j]]+1)
+			i=max(i, d[s[j]]+1) # VVVImp
 			maxEnd=j-i+1
 			res=max(maxEnd, res)
 			d[s[j]]=j
