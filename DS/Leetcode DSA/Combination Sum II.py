514e9e27946510fe9df1098bfc38fd1452e7bd88
Kiran Deep <kirandeep102030@gmail.com>
2023-02-14 22:40:55 +0530

    Added Combination Sum II

--- DS/Leetcode DSA/Combination Sum II.py
+++ DS/Leetcode DSA/Combination Sum II.py
@@ -1,23 +1,29 @@
 from typing import List
 
 class Solution:
-    def findSolution(self,i,candidates,target,ds,res):
+    def findSolution(self,index,candidates,target,ds,res):
         if target==0:
-            res.add(tuple(ds))
-            return True
+            res.append(ds.copy())
+            return
 
-        if target<0 or i==len(candidates):
-            return False
+        if index==len(candidates) or target<0:
+            return
 
-        ds.append(candidates[i])
-        resp1=self.findSolution(i+1, candidates, target-candidates[i], ds, res)
-        ds.pop(-1)
+        for i in range(index,len(candidates)):
+            if i>index and candidates[i]==candidates[i-1]:
+                continue
 
-        resp2=self.findSolution(i+1, candidates, target, ds, res)
+            if target-candidates[i]>=0:
+                ds.append(candidates[i])
+                self.findSolution(i+1, candidates, target-candidates[i], ds, res)
+                ds.pop()
+            else:
+                break
+
 
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         ds=[]
-        res=set()
+        res=[]
         self.findSolution(0,sorted(candidates),target,ds,res)
         return res
 
