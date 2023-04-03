0ab635f4d65b4247abf9367cba299564abc837d5
Kiran Deep <kirandeep102030@gmail.com>
2023-04-03 23:44:40 +0530

    Added Frog Jump

--- DS/Leetcode DSA/FrogJump.py
+++ DS/Leetcode DSA/FrogJump.py
@@ -20,6 +20,21 @@ def solve(i, height, dp):
     return dp[i]
 
 def frogJump(n: int, heights: List[int]) -> int:
+
+    # Memoization
+
     dp = [-1] * n
     res = solve(n-1, heights, dp)
     return res
+
+    # Tabulation
+
+    # def frogJump(n: int, heights: List[int]) -> int:
+    # dp = [0] * n
+    # for i in range(1,n):
+    #     left = dp[i-1] + abs(heights[i] - heights[i-1])
+    #     right = float("inf")
+    #     if i>1:
+    #         right = dp[i-2] + abs(heights[i] - heights[i-2])
+    #     dp[i] = min(left, right)
+    # return dp[n-1]
