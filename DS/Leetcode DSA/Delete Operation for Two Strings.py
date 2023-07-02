53b1b963a63f7abaf887025f37d4994e4043ac70
Kirandeep2806 <kirandeep102030@gmail.com>
2023-07-02 20:26:52 +0530

    Added Delete Operation for Two Strings

---
+++ DS/Leetcode DSA/Delete Operation for Two Strings.py
@@ -0,0 +1,16 @@
+word1 = input()
+word2 = input()
+
+m = len(word1)
+n = len(word2)
+
+dp = [[0 for _ in range(n+1)] for __ in range(m+1)]
+for i in range(1,m+1):
+	for j in range(1,n+1):
+		if word1[i-1] == word2[j-1]:
+			dp[i][j] = 1 + dp[i-1][j-1]
+		else:
+			dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+lcsCnt = dp[m][n]
+res = (m-lcsCnt) + (n-lcsCnt)
+print(res)
