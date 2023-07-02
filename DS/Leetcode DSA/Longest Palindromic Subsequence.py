5f15f6d65fc836065c77d72122f29f06432bb3c4
Kirandeep2806 <kirandeep102030@gmail.com>
2023-07-02 18:59:18 +0530

    Added even more efficient approach to LPS

--- DS/Leetcode DSA/Longest Palindromic Subsequence.py
+++ DS/Leetcode DSA/Longest Palindromic Subsequence.py
@@ -1,13 +1,33 @@
 s = input()
-t = s[::-1]
 m = len(s)
 dp = [[0 for _ in range(m+1)] for __ in range(m+1)]
-for i in range(1,m+1):
-	for j in range(1,m+1):
-		if i==0 or j==0:
-			dp[i][j] = 0
-		if s[i-1] == t[j-1]:
-			dp[i][j] = 1+dp[i-1][j-1]
-		else:
-			dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-print(dp[m][m])
+
+# Striver Approach
+
+# t = s[::-1]
+# for i in range(1,m+1):
+# 	for j in range(1,m+1):
+# 		if i==0 or j==0:
+# 			dp[i][j] = 0
+# 		if s[i-1] == t[j-1]:
+# 			dp[i][j] = 1+dp[i-1][j-1]
+# 		else:
+# 			dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+# print(dp[m][m])
+
+# Another Approach
+
+from functools import cache
+
+@cache
+def solve(i,j):
+	if i==j:
+		return 1
+	if i>j:
+		return 0
+	if s[i] == s[j]:
+		return 2 + solve(i+1,j-1)
+	else:
+		return max(solve(i+1,j),solve(i,j-1))
+res = solve(0, m-1)
+print(res)
