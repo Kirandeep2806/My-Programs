ac624d7ea2832a54bd8426b0c3d7f4c81bb21baf
Kirandeep2806 <kirandeep102030@gmail.com>
2023-07-02 20:14:45 +0530

    Added LPS Tabulation

--- DS/Leetcode DSA/Longest Palindromic Subsequence.py
+++ DS/Leetcode DSA/Longest Palindromic Subsequence.py
@@ -31,3 +31,16 @@ def solve(i,j):
 		return max(solve(i+1,j),solve(i,j-1))
 res = solve(0, m-1)
 print(res)
+
+
+# Tabulation
+
+# for i in range(n-1,-1,-1):
+# 	for j in range(i+1,n):
+# 		if i==j:
+# 			dp[i][j] = 1
+# 		elif s[i]==s[j]:
+# 			dp[i][j] = 2 + dp[i+1][j-1]
+# 		else:
+# 			dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+# print(dp[0][n-1])
