890ff4aafc94d09e91b01f1dbe3f53c6e37934fd
Kirandeep2806 <kirandeep102030@gmail.com>
2023-07-03 23:09:33 +0530

    Added Formatted Code

--- DS/Leetcode DSA/Edit Distance.py
+++ DS/Leetcode DSA/Edit Distance.py
@@ -28,7 +28,7 @@ class Solution:
             dp[0][j] = j
         for i in range(1,m+1):
             for j in range(1,n+1):
-                if s[i-1]==t[j-1]:
+                if s[i-1] == t[j-1]:
                     dp[i][j] = dp[i-1][j-1]
                 else:
                     dp[i][j] = min(dp[i][j-1], dp[i-1][j], dp[i-1][j-1]) + 1
