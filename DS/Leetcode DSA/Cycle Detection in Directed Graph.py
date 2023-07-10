50ce9fb4fcbee1ff83435447a144553c44cce63c
Kirandeep2806 <kirandeep102030@gmail.com>
2023-07-10 19:07:19 +0530

    Added code for Cycle Detection in the Direced Graph

---
+++ DS/Leetcode DSA/Cycle Detection in Directed Graph.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+# Using Kahn's Algo
+
+class Solution:
+    #Function to detect cycle in a directed graph.
+    def isCyclic(self, V, adj):
+        indegree = [0]*V
+        for i in range(V):
+            for node in adj[i]:
+                indegree[node] += 1
+        q = deque()
+        for i in range(V):
+            if indegree[i] == 0:
+                q.append(i)
+        topo = []
+        while q:
+            node = q.popleft()
+            topo.append(node)
+            for i in adj[node]:
+                indegree[i] -= 1
+                if indegree[i] == 0:
+                    q.append(i)
+        return not(len(topo) == V)
