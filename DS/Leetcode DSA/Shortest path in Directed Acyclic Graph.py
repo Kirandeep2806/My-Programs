ef7caa9fa5146cdc156c25225bc980b7e17078bf
Kirandeep2806 <kirandeep102030@gmail.com>
2023-07-12 03:05:39 +0530

    Added Shortest path in Directed Acyclic Graph

--- DS/Leetcode DSA/Shortest path in Directed Acyclic Graph.py
+++ DS/Leetcode DSA/Shortest path in Directed Acyclic Graph.py
@@ -23,15 +23,12 @@ class Solution:
 		for u,v,wt in edges:
 			adj[u].append((v,wt))
 		self.topoSort(n, adj)
-		dist = [1e9]*n # Always put is max or min value like -1e9 or 1e9 but not -1
+		dist = [1e9]*n # Always put this as max or min value like -1e9 or 1e9 but not -1
 		dist[0] = 0
 		while self.stack:
 			node = self.stack.pop()
 			for i,wt in adj[node]:
-				if dist[i] == -1:
-					dist[i] = dist[node] + wt
-				else:
-					dist[i] = min(dist[i], dist[node] + wt)
+				dist[i] = min(dist[i], dist[node] + wt)
 		for i in range(n):
 		    if dist[i] == 1e9:
 		        dist[i] = -1
