5e4fb485a543c25330242f0353df56ec47764215
Kirandeep2806 <kirandeep102030@gmail.com>
2023-07-14 21:46:27 +0530

    A slight optimization of space

--- DS/Leetcode DSA/Word Ladder.py
+++ DS/Leetcode DSA/Word Ladder.py
@@ -3,11 +3,9 @@ class Solution:
         s = set(wordList)
         if endWord not in s:
             return 0
-        vis = set()
         q = deque([(beginWord, 1)])
-        res = float("inf")
         n = len(beginWord)
-        vis.add(beginWord)
+        s.discard(beginWord)
         while q:
             word, steps = q.popleft()
             for i in range(n):
@@ -15,7 +13,7 @@ class Solution:
                     newWord = word[:i] + chr(97+char) + word[i+1:]
                     if newWord == endWord:
                         return steps + 1
-                    if (newWord not in vis) and (newWord in s):
-                        vis.add(newWord)
+                    if newWord in s:
+                        s.discard(newWord)
                         q.append((newWord, steps + 1))
         return 0
