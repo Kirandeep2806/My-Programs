518bace39a86e5814c0ea487eaf8586a44d1a2ec
Kirandeep2806 <kirandeep102030@gmail.com>
2023-07-19 13:19:59 +0530

    Added Remove nth node from end of the LL

---
+++ DS/Leetcode DSA/NewCodes/Remove Nth Node From End of List.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode()
+        dummy.next = head
+        slow = fast = dummy
+        for i in range(n):
+            fast = fast.next
+        while fast.next:
+            slow = slow.next
+            fast = fast.next
+        slow.next = slow.next.next
+        return dummy.next
